fc88262a487fd30dcd0f97dcdba01ffca17e099c
maaretp <maaret@iki.fi>
2019-08-11 19:53:24 -0400

    Response code

--- src/test/java/ExploreSwApi.java
+++ src/test/java/ExploreSwApi.java
@@ -1,19 +1,25 @@
 import static io.restassured.RestAssured.given;
 
 import org.approvaltests.Approvals;
+import org.junit.Test;
 
 import io.restassured.response.Response;
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
 
-public class ExploreSwApi extends TestCase {
+public class ExploreSwApi {
 
+		@Test
 		public void testSwapi() throws Exception {
 			 Response response = given().get("https://swapi.co/api/people/1");
 			 Approvals.verify(response.body().prettyPrint());
 		}
 		
 		
-		
+		@Test
+		public void testSwapiDelete()  {
+			 Response response = given().delete("https://swapi.co/api/people/1");
+			 assertEquals(301, response.getStatusCode());
+		}
 		
 
 	}
